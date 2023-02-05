a68ed9042026ba1f600b502ea286eb0d025e147b
Victor Torres <vatorresm88@gmail.com>
2023-02-04 21:38:37 -0500

    OPERACIÓN CON NÚMEROS - El objeto Math - PI, SQRT1_2, SQRT2

--- 03.Operaciones-Numeros/index.js
+++ 03.Operaciones-Numeros/index.js
@@ -15,3 +15,19 @@ Math.LN10
 // CONSTANTE LOG2E
 // Devuelve un valor aproximado del logaritmo en base 2 de la cosntante de Euler
 Math.LOG2E
+
+// CONSTANTE LOG10E
+// Devuelve un valor aproximado del logaritmo en base 10 de la cosntante de Euler
+Math.LOG10E
+
+// CONSTANTE PI
+// Devuelve un valor aproximado de la cosntante de PI, relación existente entre la circunferecnia de un círculo y su diámetro.
+Math.PI
+
+// CONSTANTE SQRT1_2
+// Devuelve un valor aproximado de la raíz cuadrada de un medio, es decir, de 1 sobre la raíz cuadrada de 2
+Math.SQRT1_2
+
+// CONSTANTE SQRT2
+// Devuelve un valor aproximado de la raíz cuadrada de 2
+Math.SQRT2

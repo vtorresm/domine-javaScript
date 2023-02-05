c00ef93360fd6729e66f35368b202fd4752d16b1
Victor Torres <vatorresm88@gmail.com>
2023-02-04 21:32:07 -0500

    OPERACIÓN CON NÚMEROS - El objeto Math

---
+++ 03.Operaciones-Numeros/index.js
@@ -0,0 +1,17 @@
+// EL OBJETO MATH
+
+// CONSTANTE E
+// Devuelve un valor aproximado de la constante de Euler
+Math.E
+
+// CONSTANTE LN2
+// Devuelve un valor aproximado al logaritmo neperiano de 2
+Math.LN2
+
+// CONSTANTE LN10
+// Devuelve un valor aproximado al logaritmo neperiano de 10
+Math.LN10
+
+// CONSTANTE LOG2E
+// Devuelve un valor aproximado del logaritmo en base 2 de la cosntante de Euler
+Math.LOG2E

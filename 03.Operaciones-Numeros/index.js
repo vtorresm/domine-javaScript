4fb6fe6014908788216cbe1f54e4d49ac6d9c19c
Victor Torres <vatorresm88@gmail.com>
2023-02-13 20:41:23 -0500

    OPERACIÓN CON NÚMEROS - El objeto Math - sin, sinh, sqrt

--- 03.Operaciones-Numeros/index.js
+++ 03.Operaciones-Numeros/index.js
@@ -34,87 +34,87 @@ Math.SQRT2
 
 // CONSTANTE abs
 // Devuelve el valor absoluto del valor indicado
-Math.abs(-1)
+Math.abs(-1);
 
 // CONSTANTE acos
 // Devuelve el arcocoseno del valor indicado
-Math.acos(0.999)
+Math.acos(0.999);
 
 // CONSTANTE acosh
 // Devuelve el arcocoseno hiperbólico del valor indicado
-Math.acosh(2)
+Math.acosh(2);
 
 // CONSTANTE asin
 // Devuelve el arcoseno del valor indicado
-Math.asin(0.999)
+Math.asin(0.999);
 
 // CONSTANTE asinh
 // Devuelve el arcoseno hiperbólico del valor indicado
-Math.asinh(2)
+Math.asinh(2);
 
 // CONSTANTE atan
 // Devuelve el arcoseno del valor indicado
-Math.atan(0.999)
+Math.atan(0.999);
 
 // CONSTANTE aatanh
 // Devuelve el arcoseno hiperbólico del valor indicado
-Math.atanh(0.999)
+Math.atanh(0.999);
 
 // CONSTANTE cbrt
 // Devuelve la raíz cúbica del valor indicado
-Math.cbrt(27)
+Math.cbrt(27);
 
 // CONSTANTE ceil
 // Devuelve el valor entero más pequeño redondeado hacia arriba.
-Math.ceil(-1.99)
-Math.ceil(1.99)
+Math.ceil(-1.99);
+Math.ceil(1.99);
 
 // CONSTANTE cos
 // Devuelve el el coseno del valor indicado.
-Math.cos(0)
+Math.cos(0);
 
 // CONSTANTE cosh
 // Devuelve el el coseno hiperbólico del valor indicado.
-Math.cosh(1)
+Math.cosh(1);
 
 // CONSTANTE exp
 // Devuelve la potencia de la constante de Euler elevado al valor inidcado.
-Math.exp(1)
+Math.exp(1);
 
 // CONSTANTE floor
 // Devuelve el valor entero más grande redondeado hacia abajo.
-Math.floor(-1.99)
-Math.floor(1.99)
+Math.floor(-1.99);
+Math.floor(1.99);
 
 // CONSTANTE log
 // Devuelve el logaritmo neperiano del valor indicado.
-Math.log(0)
+Math.log(0);
 
 // CONSTANTE log10
 // Devuelve el logaritmo en base 10 del valor indicado.
-Math.log10(2)
+Math.log10(2);
 
 // CONSTANTE log2
 // Devuelve el logaritmo en base 10 del valor indicado.
-Math.log2(2)
+Math.log2(2);
 
 // CONSTANTE max
 // Devuelve el mayor de los valores indicados.
-Math.max(2, 3, 5, 8, -1, 6)
+Math.max(2, 3, 5, 8, -1, 6);
 
 // CONSTANTE min
 // Devuelve el menor de los valores indicados.
-Math.min(2, 3, 5, 8, -1, 6)
+Math.min(2, 3, 5, 8, -1, 6);
 
 // CONSTANTE pow
 // Devuelve la potencia del primer valor elevado al segundo valor o parámetro.
-Math.pow(2, 3)
+Math.pow(2, 3);
 
 // CONSTANTE random
 // Devuelve un valor pseudoaleatorio entre 0 y 1.Para conseguir némeros entre un máximo y mínimo, se pueden multiplicar este resultado por la diferencia entre ambos valores y sumarle el mínimo.
 
 // Devuleve un valor decimal entre max y min
-Math.random()
+Math.random();
 // Devuleve un valor decimal entre max y min
 Math.random() * (max - min) + min
 // Devuleve un valor decimal entre max y min
@@ -122,14 +122,24 @@ Math.trunc(Math.random() * (max - min)) + min
 
 // CONSTANTE round
 // Devuelve el valor redondeado al entero más cercano.
-Math.round(-1.09)
-Math.round(1.09)
-Math.round(1.59)
+Math.round(-1.09);
+Math.round(1.09);
+Math.round(1.59);
 
 // CONSTANTE sign
 // Devuelve el signo del valor indicado expresado en forma de -1, 0 ó 1.
-Math.sign(-99)
-Math.sign(0)
-Math.sign(99)
+Math.sign(-99);
+Math.sign(0);
+Math.sign(99);
 
+// CONSTANTE sin
+// Devuelve el seno del valor indicado.
+Math.sin(1);
 
+// CONSTANTE sinh
+// Devuelve el seno del valor indicado.
+Math.sinh(1);
+
+// CONSTANTE sqrt
+// Devuelve el seno del valor indicado.
+Math.sqrt(2);

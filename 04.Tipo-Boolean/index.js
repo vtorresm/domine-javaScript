fd8ad9a7fe5bd23693932431fa6c6bae66b5e033
Victor Torres <vatorresm88@gmail.com>
2023-02-13 21:30:18 -0500

    TIPO BOOLEANO - toString

---
+++ 04.Tipo-Boolean/index.js
@@ -0,0 +1,19 @@
+//TIPO BOOLEAN
+Boolean(0);
+Boolean("");
+Boolean(null);
+Boolean(",");
+Boolean(4);
+Boolean(1) - 1 == false;
+
+/*PROPIEDADES
+//constructor
+//prototype
+*/
+
+//MÉTODOS
+
+//Método toString
+//Devuelve el booleano en formato String.
+false.toString();
+true.toString();

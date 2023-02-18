4c4f59c7d91aa1dab475cbda0ee0d216104b739d
Victor Torres <vatorresm88@gmail.com>
2023-02-17 20:40:44 -0500

    Operadores y Expresiones - OPERADOR DE ADICIÓN y SUSTRACCIÓN

--- 06.OperadoresExpresiones/index.js
+++ 06.OperadoresExpresiones/index.js
@@ -8,4 +8,14 @@ var local = new Date();
 '2' + 1;
 2 + '1';
 
+//OPERADOR ARITMÉTICOS:
 
+// OPERADOR DE ADICION
+2 + 1;
+Number(true) + 1;
+parseInt('0') + 1;
+
+// OPERADOR DE SUSTRACCIÓN
+2 - 1;
+'2' - '1';
+true - '1';

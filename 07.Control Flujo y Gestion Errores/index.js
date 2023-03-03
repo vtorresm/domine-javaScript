1962c4b855d1594df0951fb9746f666b30612e97
Victor Torres <vatorresm88@gmail.com>
2023-03-03 13:06:36 -0500

    ESTRUCTURA IF - ESTRUCTURA IF...ELSE

---
+++ 07.Control Flujo y Gestion Errores/index.js
@@ -0,0 +1,13 @@
+//ESTRUCTURA IF
+if (fecha == '20-02-2002') {
+  console.log('Es 20 de Febrero de 2002')
+}
+
+//ESTRUCTURA IF...ELSE
+if (fecha == '20-02-2002') {
+  console.log('Es 20 de Febrero de 2002')
+}else {
+  console.log('No es 20 de Febrero de 2002')
+}
+
+

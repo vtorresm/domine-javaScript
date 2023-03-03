34673335dffa6233197a05d998b2063635c982c8
Victor Torres <vatorresm88@gmail.com>
2023-03-03 13:41:59 -0500

    ESTRUCTURA SWITCH - CONTROL DE ERRORES POR TIPO DE DATO

--- 07.Control Flujo y Gestion Errores/index.js
+++ 07.Control Flujo y Gestion Errores/index.js
@@ -1,13 +1,31 @@
 //ESTRUCTURA IF
 if (fecha == '20-02-2002') {
-  console.log('Es 20 de Febrero de 2002')
+  console.log('Es 20 de Febrero de 2002');
 }
 
 //ESTRUCTURA IF...ELSE
 if (fecha == '20-02-2002') {
-  console.log('Es 20 de Febrero de 2002')
+  console.log('Es 20 de Febrero de 2002');
 }else {
-  console.log('No es 20 de Febrero de 2002')
+  console.log('No es 20 de Febrero de 2002');
 }
 
+//ESTRUCTURA SWITCH
+switch(marca) {
+  case 'ford':
+    console.log('El coche es de la marca Ford');
+    break;
+  case 'seat':
+    console.log('El coche es de la marca Seat');
+    break;
+  default:
+    console.log('El coche es de otra marca');
+}
 
+//CONTROL DE ERRORES POR TIPO DE DATO
+var fecha = '';
+if (typeof arguments[0] == 'object') {
+  fecha = new Date(aux.anio + "-" + aux.mes + "-" + aux.dia);
+} else {
+  fecha = new Date(aux);
+}
